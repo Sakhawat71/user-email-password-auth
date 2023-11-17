72476af65b39e074dbfaf7002e6bf0748b2dee77
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-18 00:37:11 +0600

    create router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const myRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <></>,
+        errorElement:<></>,
+
+    }
+]);
+
+export default myRouter;
